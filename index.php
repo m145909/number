b67bf7b3c35991df596553fb5086acdfed2af9fd
m145909 <59403636+m145909@users.noreply.github.com>
2020-05-15 20:22:50 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Number</title>
+    </head>
+    <body>
+        <p>Generating a random six-digit number:
+            <?php
+
+            echo rand(100000, 999999);
+
+            ?>
+        </p>
+    </body>
+</html>
